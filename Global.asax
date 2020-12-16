77e00a549175d631e6253f796259ea20d8805add
thomasgengel34 <thomasgengel34@yahoo.com>
2020-12-16 15:46:03 -0600

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LambNLentil.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LambNLentil.Startup))]
+namespace LambNLentil
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
